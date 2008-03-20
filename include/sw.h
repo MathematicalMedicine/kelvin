c6429bb3d5e675771eaf17517a0ea94221293e07
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2008-03-20 19:58:59 +0000

    Add trailing slash to resultspath if needed

--- include/sw.h
+++ include/sw.h
@@ -1,3 +1,11 @@
+
+/**********************************************************************
+ * Copyright 2008, Nationwide Children's Research Institute.  
+ * All rights reserved.
+ * Permission is hereby given to use this software 
+ * for non-profit educational purposes only.
+ **********************************************************************/
+
 #ifndef SW_H
 #define SW_H
 
