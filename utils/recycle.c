67834b2ce6432b02561cdafdc8504a735d2c424c
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2008-03-24 16:49:57 +0000

    Works under cygwin and linux

--- utils/recycle.c
+++ utils/recycle.c
@@ -84,7 +84,7 @@ remalloc (len, purpose)
   char *x = (char *) malloc (len);
 
   if (!x) {
-    fprintf (stderr, "malloc of %lu failed for %s\n", len, purpose);
+    fprintf (stderr, "malloc of %lu failed for %s\n", (unsigned long) len, purpose);
     exit (SUCCESS);
   }
   return x;
