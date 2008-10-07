ca42a4541ecbdd61ee07720194f6c40672e84128
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2008-10-07 19:24:40 +0000

    First version

---
+++ utils/dists.h
@@ -0,0 +1,10 @@
+double gaussian_pdf (double x, double mean, double stdDev);
+double gaussian_cdf (double x, double mean, double stdDev);
+double t_pdf_30 (double x, double degFree);
+double t_cdf_30 (double x, double degFree);
+double t_cdf (double x, double degFree);
+double chisq_pdf (double x, double degFree);
+double chisq_cdf (double x, double degFree);
+
+
+
