91f08f9ec775f75b60521ed47fcc6c7af4d89792
Sang-Cheol Seok <sangcheol.seok@nationwidechildrens.org>
2009-03-23 21:10:41 +0000

    fixing dcuhre stopping criterion

--- dcuhre.c
+++ dcuhre.c
@@ -293,7 +293,7 @@ dadhre_ (dcuhre_state * s)
     //if ((s->diff_result[s->sbrgns - 1] > s->epsabs) && (s->error > s->epsabs)) { // before 5/18/2008
     //if ((s->result <0)||(s->diff_result[s->sbrgns - 1] > s->epsabs) || real_result <0.25) {
     //if ((s->result <0)||((s->diff_result[s->sbrgns - 1] > s->epsabs) && (real_error > s->epsabs))) {  // before 11/25/2008
-    if ((s->diff_result[s->sbrgns - 1] >= s->epsabs) && ( (real_result <0.9)|| (real_error > s->epsabs) )){
+    if (((s->result <0)|| (s->diff_result[s->sbrgns - 1] >= s->epsabs)) && ( (real_result <0.9)|| (real_error > s->epsabs) )){// adding s->result <0) on 3/23/2009
       /*   If we are allowed to divide further, */
       /*   prepare to apply basic rule over each half of the */
       /*   NDIV subregions with greatest errors. */
