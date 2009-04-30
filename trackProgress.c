397c25f3ab8ca9695f1b362bb63fdf5b69c21fdd
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2009-04-30 18:41:48 +0000

    Correct naming

--- trackProgress.c
+++ trackProgress.c
@@ -262,7 +262,7 @@ char *estimateIterations (unsigned long eCL[])
 	  strcat (analysisType, "Quantitative Trait w/Threshold, ");
 	}
 	if (modelType.distrib == QT_FUNCTION_T) {
-	  strcat (analysisType, "Truncated Student's T-Distribution, Linkage ");
+	  strcat (analysisType, "Student's T-Distribution, Linkage ");
 	} else { // not T-Dist
 	  if (modelType.distrib == QT_FUNCTION_CHI_SQUARE) {
 	    strcat (analysisType, "Chi-Square Distribution, Linkage ");
@@ -315,7 +315,7 @@ char *estimateIterations (unsigned long eCL[])
 	  strcat (analysisType, "Quantitative Trait w/Threshold, ");
 	}
 	if (modelType.distrib == QT_FUNCTION_T) {
-	  strcat (analysisType, "Truncated Student's T-Distribution.");
+	  strcat (analysisType, "Student's T-Distribution.");
 	} else { // not T-Dist
 	  if (modelType.distrib == QT_FUNCTION_CHI_SQUARE) {
 	    strcat (analysisType, "Chi-Square Distribution.");
