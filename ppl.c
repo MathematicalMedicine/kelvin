7304c7af4be5d81bafe58bc413f6d35727caa1ef
Sang-Cheol Seok <sangcheol.seok@nationwidechildrens.org>
2009-06-19 20:23:18 +0000

    using kelvinGlobals.h instead of kelvinGlobalsNew.h

--- ppl.c
+++ ppl.c
@@ -6,7 +6,7 @@
  * for non-profit educational purposes only.
  **********************************************************************/
 
-#include "kelvinGlobalsNew.h"
+#include "kelvinGlobals.h"
 #include "summary_result.h"
 #include "ppl.h"
 extern LambdaCell *pLambdaCell;
