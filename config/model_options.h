9fa47825e237a6229774191c643dca080556fbf6
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2009-08-31 20:08:40 +0000

    Adding comments

--- config/model_options.h
+++ config/model_options.h
@@ -1,67 +1,86 @@
-/**********************************************************************
- * Copyright 2008, Nationwide Children's Research Institute.  
- * All rights reserved.
- * Permission is hereby given to use this software 
- * for non-profit educational purposes only.
- **********************************************************************/
-
 #ifndef __MODEL_OPTIONS_H__
 #define __MODEL_OPTIONS_H__
 
+/**
+@file model_options.h
+
+  model_options structure definition - contains all analysis
+  options parsed from the kelvin configuration file.
+
+  Currently only one instance of model_options is created; a global
+  called modelOptions. This is the sole repository of configuration
+  information, not just trait model or any other type of model. 
+  Includes definition of default values for directives.
+
+  Copyright &copy; 2009, Nationwide Children's Research Institute.  All
+  rights reserved.  Permission is hereby given to use this software
+  for non-profit educational purposes only.
+
+  @version $Id$
+
+*/
+
 #include <stdio.h>
 
-/* Maximum number of characters in an KELVIN filename. */
+/// Maximum number of characters in an KELVIN file name.
 #define KMAXFILENAMELEN 256 
 
+/// File name used for marker data if none specified in the configuration file.
 #define DEFAULTMARKERFILENAME "markers.dat"
+/// File name used for map data if none specified in the configuration file.
 #define DEFAULTMAPFILENAME "mapfile.dat"
+/// File name used for pedigree data if none specified in the configuration file.
 #define DEFAULTPEDFILENAME "pedfile.dat"
+/// File name used for marker frequency data if none specified in the configuration file.
 #define DEFAULTDATAFILENAME "datafile.dat"
+/// File name used for output of Bayes Ratio results if none specified in the configuration file.
 #define DEFAULTAVGHETFILENAME "br.out"
+/// File name used for output of PPL results if none specified in the configuration file.
 #define DEFAULTPPLFILENAME "ppl.out"
-#define DEFAULTCONDFILENAME "condL.out"
-#define DEFAULTLDPPLFILENAME "ldppl.out"
+/// Path name used for locating saved results if none specified in the configuration file.
 #define DEFAULTRESULTSPREFIX "./"
 
-/* Analysis mode (LE or LD) */
+/// model_options.equilibrium value indicating linkage equilibrium analysis mode
 #define LINKAGE_EQUILIBRIUM       0
+/// model_options.equilibrium value indicating linkage disequilibrium analysis mode
 #define LINKAGE_DISEQUILIBRIUM    1
 
-/* Marker to marker analysis types. FALSE means no analysis. */
-#define MARKERTOMARKER            1   /* marker to marker analysis */
+/// model_options.markerAnalysis value indicating 2pt marker-to-marker for all possible pairs of markers
+#define MARKERTOMARKER            1
 #define MM MARKERTOMARKER
-#define ADJACENTMARKER            2   /* marker to adjacent marker only analysis */
+/// model_options.markerAnalysis value indicating 2pt marker-to-marker for adjacent markers only
+#define ADJACENTMARKER            2
 #define AM ADJACENTMARKER
 
-/* Specifiy how the map should be used */
-#define SEX_AVERAGED              0   /* analysis uses sex-averaged map positions */
+/// model_options.mapFlag value indicating analysis uses sex-averaged map positions.
+#define SEX_AVERAGED              0
 #define SA SEX_AVERAGED
-#define SEX_SPECIFIC              1   /* analysis uses sex-specific map positions */
+/// model_options.mapFlag value indicating analysis uses sex-specific map positions.
+#define SEX_SPECIFIC              1
 #define SS SEX_SPECIFIC
 
-/* Affection status for a dichotomous trait for a person */
+/// Phenotype code (affection status) unknown for a dichotomous trait.
 #define AFFECTION_STATUS_UNKNOWN        0
+/// Phenotype code (affection status) for a dichotomous trait known to be unaffected.
 #define AFFECTION_STATUS_UNAFFECTED     1
+/// Phenotype code (affection status) for a dichotomous trait known to be affected.
 #define AFFECTION_STATUS_AFFECTED       2
 
 typedef struct ModelOptions
 {
-  /* analysis type - Linkage Equalibrium (LE) or Linkage Disequalibrium (LD) */
-  int equilibrium;
+  int equilibrium;  //< Analysis type - LINKAGE_EQUALIBRIUM or LINKAGE_DISEQUILIBRIUM
+  int polynomial;  //< Flag to indicate if polynomial representation is being used.
+  int polynomialScale;	//< Optional scaling factor for polynomial construction storage
+  int integration;      //< Flag to indicate if dynamic grid (integration) is being performed.
+  int maxIterations;    //< Optional upper limit the number of per-BR dynamic grid iterations
+  int sexLinked;   //< Flag to indicate if trait is sex-linked
 
-  int polynomial;		/* TRUE if using polynomial to represent likelihood */
-  int polynomialScale;		/* Optional argument to PE directive to set initial memory size */
-
-  int integration;              /* TRUE if using dkelvin integration method */
-  int maxIterations;            /* limit the number of per-BR dKelvin iterations */
-
-  /* whether the chromosome is X chromosome
+  /* &&& whether the chromosome is X chromosome
    * For some segments mainly at the telomeres, they can cross over with Y chromosome
    * so they behave like an autosomal segment, we call it pseudo autosomal regions
    * For X chromosome, male (XY) 's X chromosome will be doubled as 
    * homozygotes, so to handle both males and females the same way 
    */
-  int sexLinked;
 
   /* whether to use sex-averaged or sex-specific map: only for
    * QT/CT. */
