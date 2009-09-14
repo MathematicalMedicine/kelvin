bf6fbbfce0d649ec6594a15a489583d14b1dbf94
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2009-09-14 16:00:56 +0000

    Document pageManagement.c

--- utils/pageManagement.c
+++ utils/pageManagement.c
@@ -1,9 +1,36 @@
+/**
+@file pageManagement.c
+
+  Page-at-a-time replacement for (m|c)alloc that allows us to read-lock global variables.
+
+  Since we're not going to use C++ and therefore can't have restricted accessors for the
+  global variables that would otherwise be objects, we do the next best thing by allocating
+  space for these global variables a page and a time and using mprotect() to mark them
+  as read-only once they're initialized.
+
+  A good example of usage is in config/config.c, where the signal handler is setup and at
+  least some configuration-related global variables allocated and read-locked. This is, of
+  course, primarily a development tool to identify bad behavior, so it could be conditionalized
+  away for production runs.
+
+  Copyright &copy; 2009, Nationwide Children's Research Institute.  All
+  rights reserved.  Permission is hereby given to use this software
+  for non-profit educational purposes only.
+
+  @version $Id$
+*/
+  
 #include <unistd.h>
 #include <sys/mman.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+  The signals for violations of memory protection depend upon 
+  OS lineage. Somehow I felt that this should be __BSD__, but
+  nothing like that exists.
+*/
 #ifdef __APPLE__
   #define FAULT_NAME SIGBUS
 #else
@@ -19,6 +46,26 @@ static void segvHandler (int signum, siginfo_t *sigi, void *unused)
   }
 }
 
+
+/**
+
+  Setup the handler for bus errors for segmentation violations
+  that we might cause by read-protecting our globals.
+
+  @author Bill Valentine-Cooper - overall content.
+  @par Reviewers:
+
+  @par Global Inputs
+
+  none
+
+  @par Global Outputs
+
+  none
+
+  @return Nothing
+
+*/
 void setupSegvHandler (void)
 {
   struct sigaction segvAction;
@@ -30,7 +77,27 @@ void setupSegvHandler (void)
   }
 }
 
-void *allocatePages (int objectSizeInBytes)
+/**
+
+  Allocate a series of contiguous pages large enough to hold the number
+  of bytes specified and return a pointer to the start address.
+
+  @author Bill Valentine-Cooper - overall content.
+
+  @par Global Inputs
+
+  Process address space
+
+  @par Global Outputs
+
+  A little less free address space
+
+  @return a pointer to the start of the pages allocated, exits with EXIT_FAILURE if the allocation fails.
+
+*/
+void *allocatePages (
+		     int objectSizeInBytes ///< Number of bytes to allocate
+		     )
 {
   void *pageStart;
   int pageSize, pageCount;
@@ -51,18 +118,59 @@ void *allocatePages (int objectSizeInBytes)
   return pageStart;
 }
 
-void allowReadOnly (void *pageStart, int objectSizeInBytes)
+/**
+
+  Change protection for the page(es) specified to allow only read references.
+
+  @author Bill Valentine-Cooper - overall content.
+
+  @par Global Inputs
+
+  None
+
+  @par Global Outputs
+
+  None
+
+  @return none, exits with EXIT_FAILURE if the mprotect fails.
+
+*/
+void allowReadOnly (
+		    void *pageStart, ///< Starting address of pages to be protected
+		    int objectSizeInBytes ///< Size of region to protect in bytes, will convert to pages
+)
 {
   int pageSize, pageCount;
 
   pageSize = getpagesize ();
   pageCount = objectSizeInBytes / pageSize + 1;
 
-  if (mprotect (pageStart, pageCount, PROT_READ) == -1)
+  if (mprotect (pageStart, pageCount, PROT_READ) == -1) {
     perror ("mprotect");
+    exit (EXIT_FAILURE);
+  }
 }
 
-void allowReadWrite (void *pageStart, int objectSizeInBytes)
+/**
+
+  Change protection for the page(es) specified to allow read and write access.
+
+  @author Bill Valentine-Cooper - overall content.
+
+  @par Global Inputs
+
+  None
+
+  @par Global Outputs
+
+  None
+
+  @return none, exits with EXIT_FAILURE if the mprotect fails.
+
+*/
+void allowReadWrite (
+		    void *pageStart, ///< Starting address of pages to be protected
+		    int objectSizeInBytes ///< Size of region to protect in bytes, will convert to pages
 {
   int pageSize, pageCount;
 
