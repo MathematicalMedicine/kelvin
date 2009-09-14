c7b1298e805d2535f03b31e1ab3e8c6aa73cef57
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2009-09-14 16:26:47 +0000

    Document pageManagement.c

--- utils/pageManagement.h
+++ utils/pageManagement.h
@@ -1,6 +1,19 @@
 #ifndef __PAGE_MANAGEMENT__
 #define __PAGE_MANAGEMENT__
 
+/**
+@file pageManagement.h
+
+  @see pageManagement.c
+  
+  Copyright &copy; 2009, Nationwide Children's Research Institute.  All
+  rights reserved.  Permission is hereby given to use this software
+  for non-profit educational purposes only.
+
+  @version $Id$
+
+*/
+
 void setupSegvHandler (void);
 void *allocatePages (int objectSizeInBytes);
 void allowReadOnly (void *object, int objectSizeInBytes);
