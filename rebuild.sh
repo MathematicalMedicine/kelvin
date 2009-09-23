efe88399f62e43bf1460c19e6c5ae5b4d79d9773
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2009-09-23 11:58:58 +0000

    Bail on warnings here too

--- rebuild.sh
+++ rebuild.sh
@@ -1,38 +1,38 @@
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -fopenmp -DMEMGRAPH" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -fopenmp -DMEMGRAPH" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-normal
 
 # Set OMP_NUM_THREADS=<something big> for best performance after compiliation of DLs.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -fopenmp -DMEMGRAPH -DPOLYUSE_DL" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -fopenmp -DMEMGRAPH -DPOLYUSE_DL" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYUSE_DL
 
 # Best reliable method for preparing code for DLs. Need to compile separately and then evaluate.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYCODE_DL-FAKEEVALUATE
 
 # Best experimental method for preparing LARGE DLs. Need to compile separately then evaluate.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYSTATISTICS -DUSE_SSD -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYSTATISTICS -DUSE_SSD -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYCODE_DL-FAKEEVALUATE-SSD
 
 # Diagnostic for DLs.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYCODE_DL -DPOLYCOMP_DL -DPOLYCHECK_DL" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYCODE_DL -DPOLYCOMP_DL -DPOLYCHECK_DL" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYCHECK_DL
 
 # Build, code and then compile and evaluate DLs. Works fine for small polynomials.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYCOMP_DL
 
 # Build, code and then compile and evaluate DLs. Works fine for small polynomials.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make CFLAGS="$* -Wall -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
+make CFLAGS="$* -Wall -Werror -DGCCOPT=3 -O3 -DMEMGRAPH -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl" kelvin
 mv kelvin kelvin-POLYCOMP_DL-FAKEEVALUATE
