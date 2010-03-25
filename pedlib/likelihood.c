6533236eabaa7a49cc8d789e2a3c9f22fb41c90a
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2010-03-25 15:40:44 +0000

    Eliminate (most) redundant WARNINGs and drop a CR from 'thats too small' message

--- pedlib/likelihood.c
+++ pedlib/likelihood.c
@@ -405,7 +405,7 @@ int compute_likelihood (PedigreeSet * pPedigreeList)
       if (modelOptions->dryRun == 0) {
         if (pPedigree->likelihood == 0.0) {
 	  if (!modelRange->atypicalQtTrait)
-	    WARNING ("Pedigree %s has likelihood %G thats too small.\n", pPedigree->sPedigreeID, pPedigree->likelihood);
+	    WARNING ("Pedigree %s has likelihood %G thats too small.", pPedigree->sPedigreeID, pPedigree->likelihood);
           if (modelOptions->polynomial)
             DIAG (LIKELIHOOD, 1, {fprintf (stderr, "Polynomial terms (depth of 1):\n"); expTermPrinting (stderr, pPedigree->likelihoodPolynomial, 1); fprintf (stderr, "\n");});
           ret = -1;
