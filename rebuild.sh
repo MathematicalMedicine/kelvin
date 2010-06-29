b14434fa2fe761a805d5e236096992a1cfa8ef8f
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2010-06-29 19:00:22 +0000

    Trimmed, recommended opt and -g'd

--- rebuild.sh
+++ rebuild.sh
@@ -19,7 +19,7 @@ fi
 case $HOSTNAME in
     Levi-Montalcini* )
 	PTMALLOC3="-lptmalloc3 -lpthread"
-        WERROR=-Werror
+#        WERROR=-Werror
         ;;
     * )
 	PTMALLOC3=
@@ -35,46 +35,29 @@ fi
 VERSION="`cat .maj`.`cat .min`.`cat .pat`"
 
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH" ADD_LDFLAGS="-ldl $PTMALLOC3" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH" ADD_LDFLAGS="-ldl $PTMALLOC3" kelvin
 mv kelvin-$VERSION kelvin-$VERSION-no_GSL
-
-# Set OMP_NUM_THREADS=<something big> for best performance after compiliation of DLs.
-make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
-mv kelvin-$VERSION kelvin-$VERSION-POLYUSE_DL
 
 # Best reliable method for preparing code for DLs. Need to compile separately and then evaluate.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE  -I/usr/local/mysql/include -I/usr/include/mysql" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm  -lklvndb -lmysqlclient -L/usr/local/mysql/lib -L/usr/lib64/mysql"  kelvin
 mv kelvin-$VERSION kelvin-$VERSION-POLYCODE_DL-FAKEEVALUATE
 
 # Best experimental method for preparing LARGE DLs. Need to compile separately then evaluate.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DUSE_SSD -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DUSE_SSD -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE -I/usr/local/mysql/include -I/usr/include/mysql" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm  -lklvndb -lmysqlclient -L/usr/local/mysql/lib -L/usr/lib64/mysql"  kelvin
 mv kelvin-$VERSION kelvin-$VERSION-POLYCODE_DL-FAKEEVALUATE-SSD
 
-# Diagnostic for DLs.
-# Notice no OpenMP, as it gains no speed and loses us lots of memory.
-make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL -DPOLYCODE_DL -DPOLYCOMP_DL -DPOLYCHECK_DL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
-mv kelvin-$VERSION kelvin-$VERSION-POLYCHECK_DL
-
 # Build, code and then compile and evaluate DLs. Works fine for small polynomials.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
 mv kelvin-$VERSION kelvin-$VERSION-POLYCOMP_DL
 
-# Build, code and then compile and evaluate DLs. Works fine for small polynomials.
-# Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL -DPOLYSTATISTICS -DPOLYCODE_DL -DPOLYCOMP_DL -DFAKEEVALUATE" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
-mv kelvin-$VERSION kelvin-$VERSION-POLYCOMP_DL-FAKEEVALUATE
-
-make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O3 -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL" ADD_LDFLAGS="-ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
 cp kelvin-$VERSION kelvin-$VERSION-normal
 
 make seq_update/calc_updated_ppl
