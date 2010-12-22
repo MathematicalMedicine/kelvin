2fb6d56ef080bd201c5db0127259cbbdabb0878a
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2010-12-22 16:57:30 +0000

    Out-of-order builds for leaving a regular kelvin lying about

--- rebuild.sh
+++ rebuild.sh
@@ -35,24 +35,19 @@ fi
 VERSION="`cat .maj`.`cat .min`.`cat .pat`"
 
 make clean
-make USE_GSL=no USE_OPENMP=no $* kelvin
+make USE_GSL=no USE_OPENMP=no $* kelvin-$VERSION
 mv kelvin-$VERSION kelvin-$VERSION-no_GSL
 
 # Set OMP_NUM_THREADS=<something big> for best performance after compiliation of DLs, doesn't do compilation if DL not found.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin-$VERSION
 mv kelvin-$VERSION kelvin-$VERSION-POLYUSE_DL
 
 # Build, code and then compile and evaluate DLs. Works fine for small polynomials.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DPOLYCOMP_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin-$VERSION
 mv kelvin-$VERSION kelvin-$VERSION-POLYCOMP_DL
-
-# Normal every-day use
-make clean
-make $* kelvin
-cp kelvin-$VERSION kelvin-$VERSION-normal
 
 if test "$HOSTNAME" = "Levi-Montalcini" ; then
     # Likelihood server build.
@@ -60,5 +55,10 @@ if test "$HOSTNAME" = "Levi-Montalcini" ; then
     make  USE_STUDYDB=yes $* kelvin
     mv kelvin-$VERSION kelvin-$VERSION-study
 fi
+
+# Normal every-day use
+make clean
+make $* kelvin
+cp kelvin-$VERSION kelvin-$VERSION-normal
 
 make seq_update/calc_updated_ppl
