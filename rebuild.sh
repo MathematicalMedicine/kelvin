b86ad33d11d0dec7cbd71395a675aff5da1371f5
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2010-12-22 16:28:57 +0000

    Clean-up version construction

--- rebuild.sh
+++ rebuild.sh
@@ -35,26 +35,13 @@ fi
 VERSION="`cat .maj`.`cat .min`.`cat .pat`"
 
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH" ADD_LDFLAGS="-g -ldl $PTMALLOC3" kelvin
+make USE_GSL=no USE_OPENMP=no $* kelvin
 mv kelvin-$VERSION kelvin-$VERSION-no_GSL
 
 # Set OMP_NUM_THREADS=<something big> for best performance after compiliation of DLs, doesn't do compilation if DL not found.
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin-$VERSION
+make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL -DPOLYUSE_DL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
 mv kelvin-$VERSION kelvin-$VERSION-POLYUSE_DL
-
-# Best reliable method for preparing code for DLs. Need to compile separately and then evaluate.
-# Notice no OpenMP, as it gains no speed and loses us lots of memory.
-#make clean
-#make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE  -I/usr/local/mysql/include -I/usr/include/mysql" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm  -lklvndb -lmysqlclient -L/usr/local/mysql/lib -L/usr/lib64/mysql"  kelvin
-#make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE"  ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm "  kelvin
-#mv kelvin-$VERSION kelvin-$VERSION-POLYCODE_DL-FAKEEVALUATE
-
-# Best experimental method for preparing LARGE DLs. Need to compile separately then evaluate.
-# Notice no OpenMP, as it gains no speed and loses us lots of memory.
-#make clean
-#make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DMEMGRAPH -DUSE_GSL -DPOLYSTATISTICS -DUSE_SSD -DPOLYUSE_DL -DPOLYCODE_DL -DFAKEEVALUATE"  ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm "  kelvin
-#mv kelvin-$VERSION kelvin-$VERSION-POLYCODE_DL-FAKEEVALUATE-SSD
 
 # Build, code and then compile and evaluate DLs. Works fine for small polynomials.
 # Notice no OpenMP, as it gains no speed and loses us lots of memory.
@@ -64,13 +51,13 @@ mv kelvin-$VERSION kelvin-$VERSION-POLYCOMP_DL
 
 # Normal every-day use
 make clean
-make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT $OPENMP -DMEMGRAPH -DUSE_GSL" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm" kelvin
+make $* kelvin
 cp kelvin-$VERSION kelvin-$VERSION-normal
 
 if test "$HOSTNAME" = "Levi-Montalcini" ; then
     # Likelihood server build.
     make clean
-    make $* CFLAGS=" $ADD_CFLAGS -Wall $WERROR -DGCCOPT=2 -O2 -g -D_REENTRANT -DUSE_GSL -DUSE_STUDYDB -DSTUDYDB -I/usr/local/mysql/include -I/usr/include/mysql" ADD_LDFLAGS="-g -ldl $PTMALLOC3 -lgsl -lgslcblas -lm -lrt -lklvndb -lmysqlclient -L/usr/local/mysql/lib -L/usr/lib64/mysql" kelvin
+    make  USE_STUDYDB=yes $* kelvin
     mv kelvin-$VERSION kelvin-$VERSION-study
 fi
 
