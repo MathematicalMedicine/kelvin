4288240db1b954e792abe72514e5a267f0e70c80
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2011-02-10 18:26:40 +0000

    Missed very precise markers on sizing join for LKS

--- iterationSupport.c
+++ iterationSupport.c
@@ -1073,7 +1073,7 @@ void iterateMain ()
       analysisLocusList->traitLocusIndex = traitIndex;
       analysisLocusList->traitOrigLocus = traitLocus;
 
-      SUBSTEP (((posIdx * 100) / numPositions), "Starting w/trait locus at %.2f (%d/%d positions)", traitPos, posIdx + 1, numPositions);
+      SUBSTEP (((posIdx * 100) / numPositions), "Starting w/trait locus at %.4g (%d/%d positions)", traitPos, posIdx + 1, numPositions);
 
       markerSetChanged = FALSE;
       if (prevFirstMarker != mp_result[posIdx].pMarkers[0] || prevLastMarker != mp_result[posIdx].pMarkers[modelType->numMarkers - 1]) {
