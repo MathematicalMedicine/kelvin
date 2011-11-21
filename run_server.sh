b864c75d8b154cf3b0312a4e362aaa96d032976a
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2011-11-21 19:02:35 +0000

    Move over from bcmmtools and reference current path

---
+++ run_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$(dirname $0)/kelvin-study $1.conf --ProgressLevel 2 --ProgressDelaySeconds 0 &
+study=$(grep -i ^Study $1.conf)
+set -- $study
+kid=$!
+wait $kid
+kidStatus=$?
+mysql --host $4 --user $6 --password=$7 $5 --execute="Update Servers set StopTime = Now(), ExitStatus = $kidStatus where HostName = '`hostname`' AND ProcessId = $kid;"
