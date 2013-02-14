0cbe3cc084972d799f151b1fc9f88ae7754e5be7
John Burian <john.burian@nationwidechildrens.org>
2013-02-14 21:57:58 +0000

    moved to LKS/

--- remove_study.sh
+++
@@ -1,4 +0,0 @@
-# Grab STUDY directive for database parameters
-study=$(grep -i ^Study client.conf)
-set -- $study
-Status=$(mysql --host $4 --user $6 --password=$7 $5 --batch --skip-column-names --execute="call CleanStudy('$2');")
