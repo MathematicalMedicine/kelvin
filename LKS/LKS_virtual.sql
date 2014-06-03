12deae89019bf370956f41cfebc81b882e9b6ffb
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2014-06-03 19:43:09 +0000

    Make it work for local use

--- LKS/LKS_virtual.sql
+++ LKS/LKS_virtual.sql
@@ -1,3 +1,6 @@
-Grant all on *.* to 'adminLKS'@'%' identified by 'barfewww';
-Grant all on *.* to 'adminLKS'@'localhost' identified by 'barfewww';
 Create database LKS;
+use mysql;
+Delete from user where User = '';
+Grant all on *.* to 'adminLKS'@'localhost' identified by 'barfewww';
+Grant all on *.* to 'adminLKS'@'%' identified by 'barfewww';
+flush privileges;
