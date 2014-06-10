de7c823d4d32986d9ad81e013dbbcc0bdaf91a8b
Bill Valentine-Cooper <william.valentine-cooper@nationwidechildrens.org>
2014-06-10 12:18:04 +0000

    Use InitStudy.pl where it lies

--- LKS/cycle.sh
+++ LKS/cycle.sh
@@ -40,8 +40,8 @@ esac
 # Do the initialization only if there was no command line parameter
 if test -z "$1" ; then
     # Setup database tables
-    perl $KELVIN_ROOT/InitStudy.pl client.conf
-    perl $KELVIN_ROOT/InitStudy.pl server.conf
+    perl $KELVIN_ROOT/LKS/InitStudy.pl client.conf
+    perl $KELVIN_ROOT/LKS/InitStudy.pl server.conf
 
     # Initial full run of client
     qrsh "cd `pwd`; $KELVIN_ROOT/kelvin-study client.conf --ProgressLevel 2 --ProgressDelaySeconds 0"
