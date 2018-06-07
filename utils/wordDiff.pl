88212dbae6623f7fec765d34c1075f7b504acedc
Jo Valentine-Cooper <jo.valentine-cooper@nationwidechildrens.org>
2018-06-07 13:25:29 +0000

    reconciling changes between different production versions of wordDiff.pl

--- utils/wordDiff.pl
+++ utils/wordDiff.pl
@@ -1,4 +1,8 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
+
+# FIXME: why isn't 'use strict;' present?
+use warnings;
+
 #
 # Word-oriented file comparison with case folding and a numeric slop factor.
 #
@@ -8,6 +12,7 @@
 # All rights reserved. Permission is granted to use this software for
 # non-profit educational purposes only.
 #
+# $Id$
 
 if ($#ARGV < 2) {
     print "Usage: ".$ARGV[0]." <file> <file> <maximum numeric error factor, 0 for equality>\n";
@@ -17,6 +22,15 @@ if ($#ARGV < 2) {
 $file_LEFT = shift;
 $file_RIGHT = shift;
 $maxOffByFactor = shift;
+# verify that files exist before we proceed
+unless (-e $file_LEFT) {
+    print "File $file_LEFT not found.\n";
+    exit (1);
+}
+unless (-e $file_RIGHT) {
+    print "File $file_RIGHT not found.\n";
+    exit (1);
+}
 open LEFT, $file_LEFT; open RIGHT, $file_RIGHT;
 $whole_LEFT = do { local $/; <LEFT> }; # Briefly make the special variable for line delimiter undefined...
 $whole_RIGHT = do { local $/; <RIGHT> }; # ...so we can slurp-in the whole file at once.
@@ -38,30 +52,30 @@ for ($i = 0; $i < $#chunks_LEFT; $i++) {
     next if ($left =~ /V\S+/ && $right =~ /V\S+/); # Ignore comparisons of version numbers (e.g. V2.3)
     next if ($left =~ /^\s*$/ && $right =~ /^\s*$/); # chunks of contiguous whitespace always match
     if ($left ne $right) {
-	# Not a textual match
-	if ($left =~ /^[-+]?[0-9]+[\.]?[0-9]*([Ee][+-][0-9]*)?$/) {
-	    # It's numeric, try simple equality to avoid e+/-00 issues.
-	    if ($left != $right) {
-		# Not simply equal, get the deviation and compare to maximum allowable
-		if ($left != 0 and $right != 0) {
-		    $offByFactor = ($left-$right)/$left;
-		} else {
-		    $offByFactor = $right+$left;
-		}
-#		print "Off by $offByFactor vs limit of $maxOffByFactor\n";
-		if (abs($offByFactor) > $maxOffByFactor) {
-		    print "Difference of factor of $offByFactor between $left and $right ".
-			"is over limit of $maxOffByFactor\n";
-		    exit (1);
-		}
-	    }
-	} else {
-	    # Non-numeric match problem, try case folding
-	    if (lc($left) ne lc($right)) {
-		print "Difference between [$left] and [$right]\n";
-		exit (1);
-	    }
-	}
+        # Not a textual match
+        if ($left =~ /^[-+]?[0-9]+[\.]?[0-9]*([Ee][+-][0-9]*)?$/) {
+            # It's numeric, try simple equality to avoid e+/-00 issues.
+            if ($left != $right) {
+                # Not simply equal, get the deviation and compare to maximum allowable
+                if ($left != 0 and $right != 0) {
+                    $offByFactor = ($left-$right)/$left;
+                } else {
+                    $offByFactor = $right+$left;
+                }
+#                print "Off by $offByFactor vs limit of $maxOffByFactor\n";
+                if (abs($offByFactor) > $maxOffByFactor) {
+                    print "Difference of factor of $offByFactor between $left and $right ".
+                        "is over limit of $maxOffByFactor\n";
+                    exit (1);
+                }
+            }
+        } else {
+            # Non-numeric match problem, try case folding
+            if (lc($left) ne lc($right)) {
+                print "Difference between [$left] and [$right]\n";
+                exit (1);
+            }
+        }
     }
 }
 exit 0;
