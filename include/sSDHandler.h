943742844945865ce410dee9ae4ceab4f4e71e3b
Jo Valentine-Cooper <jo.valentine-cooper@nationwidechildrens.org>
2019-03-29 14:38:33 +0000

    SVN/Source Directories Reconcile, part 1: there were a number of checked out working copies of releases in our old source dir that for some reason had never committed the header files in include/. putting those in for the benefit of the historical record

---
+++ include/sSDHandler.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct chunkTicket {
+  unsigned long doublePairCount; // Size in double pairs of this chunk
+  unsigned long chunkOffset; /* Offset in double pairs from beginning of file, max of 
+				64Gb/16b = 4M, so largest usable SSD is 4M * 32K = 128G. */
+};
+
+void initSSD ();
+void statSSD ();
+void termSSD ();
+
+// The smallest chunk to split, in double pairs, trying 2^8 or 256 (2^MIN_USE_SSD_BITS)
+#ifdef MAIN
+#define MIN_USE_SSD_BITS 8
+#else
+#define MIN_USE_SSD_BITS 8
+#endif
+#define MIN_USE_SSD_DPS (1<<MIN_USE_SSD_BITS)
+
+// Maximum size of a chunk, in double pairs, 2^15=32K, but we really can go beyond that
+#ifdef MAIN
+#define MAX_DPC_MASK 0x7FFFF
+#else
+#define MAX_DPC_MASK 0x7FFFF
+#endif
+
+
+struct chunkTicket *putSSD (double *buffer, unsigned long myDPC);
+void getSSD (struct chunkTicket *myTicket, double *buffer);
+void freeSSD (struct chunkTicket *myTicket);
+void flushSSD (double *buffer, unsigned long chunkOffset, unsigned long doublePairCount);
+
---
+++ include/sw.h
@@ -0,0 +1,71 @@
+
+/**********************************************************************
+ * Copyright 2008, Nationwide Children's Research Institute.  
+ * All rights reserved.
+ * Permission is hereby given to use this software 
+ * for non-profit educational purposes only.
+ **********************************************************************/
+
+#ifndef SW_H
+#define SW_H
+
+#include <sys/resource.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSWNAME 32
+#define MAXSWMSG 220
+#define MAXUDPMSG 230
+
+void pushStatus (char program, char *currentStatus);
+void popStatus (char program);
+
+struct swStopwatch
+{
+  char swName[MAXSWNAME + 1];
+  struct rusage swStartRUSelf;
+  struct rusage swStartRUChildren;
+  time_t swStartWallTime;
+  struct rusage swAccumRUSelf;
+  struct rusage swAccumRUChildren;
+  time_t swAccumWallTime;
+  int swStartedCount;
+  int swRunning;
+};
+
+struct swStopwatch *swCreate (char *);
+void swStart (struct swStopwatch *);
+void swStop (struct swStopwatch *);
+void swDumpM (struct swStopwatch *);
+void swDump (struct swStopwatch *);
+void swReset (struct swStopwatch *);
+
+void swLogMsg (char *);
+int udpSend (char *, int, char *);
+
+void *swMalloc (size_t, char *, int);
+void *swCalloc (size_t, int, char *, int);
+void *swRealloc (void *, size_t, char *, int);
+void swFree (void *, char *, int);
+void swDumpHeldTotals ();
+void swDumpBlockUse ();
+void swAddChunk (void *, size_t, int, char *, int);
+size_t swDelChunk (void *, int, char *, int);
+void swDumpSources ();
+void swDumpCrossModuleChunks ();
+void swLogPeaks (char *);
+int swGetMaximumPMK ();
+int swGetCurrentVMK (pid_t);
+
+#ifdef DMUSE
+extern int used24s, used48s, used100s, missed24s, missed48s, missed100s;
+#endif
+#ifdef DMTRACK
+extern double totalMalloc, totalFree, totalReallocOK, totalReallocMove,
+  totalReallocFree, currentAlloc, peakAlloc;
+extern int countMalloc, countFree, countReallocOK, countReallocMove,
+  countReallocFree, maxListDepth, maxRecycles;
+#endif
+
+#endif
