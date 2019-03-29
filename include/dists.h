d70f3e678093c4dad74e1246003ab861b9019d0c
Jo Valentine-Cooper <jo.valentine-cooper@nationwidechildrens.org>
2019-03-29 14:38:33 +0000

    SVN/Source Directories Reconcile, part 1: there were a number of checked out working copies of releases in our old source dir that for some reason had never committed the header files in include/. putting those in for the benefit of the historical record

---
+++ include/dists.h
@@ -0,0 +1,12 @@
+double gaussian_pdf (double x, double mean, double stdDev);
+double ugaussian_pdf (double x);
+double gaussian_cdf (double x, double mean, double stdDev);
+double ugaussian_cdf (double x);
+double t_pdf_30 (double x, double degFree);
+double t_cdf_30 (double x, double degFree);
+double t_cdf (double x, double degFree);
+double chisq_pdf (double x, double degFree);
+double chisq_cdf (double x, double degFree);
+
+
+
