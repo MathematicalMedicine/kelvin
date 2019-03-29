8a8ea9090d7d99256ce20f00770f88d1d6d542ea
Jo Valentine-Cooper <jo.valentine-cooper@nationwidechildrens.org>
2019-03-29 15:21:00 +0000

    SVN/Source Directories Reconcile, part 3: source directories representing releases that were never tagged as such in svn (and in some cases never reached svn at all). this includes two -exportsrc releases that were ostensibly tagged but had such extensive differences that I couldn't reconcile them. all releases were subject to a 'make clean' before being added here

---
+++ kelvin.h
@@ -0,0 +1,246 @@
+/**********************************************************************
+ * Multiprocessor Linkage Analysis
+ * Alberto Maria Segre, Yungui Huang
+ * RADSMM storage code Martin Milder
+ * 
+ * Copyright 2006, The University of Iowa.  All rights reserved.
+ * Permission is hereby given to use and reproduce this software 
+ * for non-profit educational purposes only.
+ **********************************************************************/
+#include <stdio.h>		/* Needed for printf */
+#include <stdlib.h>		/* EXIT_SUCCESS, EXIT_ERROR */
+#include <unistd.h>		/* Needed for getpid */
+#include <string.h>
+#include <time.h>		/* Needed for sclock */
+#include <signal.h>		/* Needed for signal */
+#include <fcntl.h>		/* Needed for open */
+#include <limits.h>		/* USHRT_MAX */
+#include <math.h>		/* for calculating logs */
+//#include <bits/nan.h>		/* NAN (not included by math.h?) */ 
+#if FALSE
+#include "niceaux.h"		/* NICE utilities. */
+#include "nicecom.h"
+#include "niceapi.h"
+#endif
+#include "utils.h"		/* Kelvin utilities. */
+#include "pedlib.h"		/* Pedigree library. */
+#ifdef RADSMM
+#include "RADSMM.h"		/* RADSMM library. */
+#endif
+
+/**********************************************************************
+ * Some internal defines.
+ **********************************************************************/
+/* Maximum number of characters in an KELVIN filename. */
+#define KMAXFILENAMELEN 64
+/* Maximum number of characters in a KELVIN input line. */
+#define KMAXLINELEN 1024
+
+/**********************************************************************
+ * Nagging parameters.
+ **********************************************************************/
+/* Sleep interval when your master tells you to back off. */
+#define NAGSLEEP	60	/* In seconds. */
+
+/* Nagging messages. */
+#define NQIDIDLE    	1	/* I need a problem to work on. */
+#define NQIDSOLN	2	/* I have some solution values. */
+#define NQIDABORT	3	/* Master wants me to abort. */
+
+/**********************************************************************
+ * Checkpointing parameters.
+ **********************************************************************/
+#ifdef DEBUG
+#define CKPTINT    	600	/* In seconds. */
+#else
+#define CKPTINT		3600	/* In seconds. */
+#endif
+
+/**********************************************************************
+ * Progress meters.
+ **********************************************************************/
+extern unsigned long int nLodsTotal;
+extern unsigned long int nLodsSlave;
+extern unsigned long int nLodsRoot;
+
+/**********************************************************************
+ * Input files. These are not saved in the model structure because
+ * they are really only ever needed by the root process.
+ ***********************************************************************/
+extern char mapfile[KMAXFILENAMELEN + 1];
+extern char markerfile[KMAXFILENAMELEN + 1];
+extern char pedfile[KMAXFILENAMELEN + 1];
+extern char datafile[KMAXFILENAMELEN + 1];
+#if FALSE
+extern char loopsfile[KMAXFILENAMELEN + 1];
+#endif
+extern char outfile[KMAXFILENAMELEN + 1];
+extern char avghetfile[KMAXFILENAMELEN + 1];
+extern char avghomofile[KMAXFILENAMELEN + 1];
+extern char pplfile[KMAXFILENAMELEN + 1];
+
+/**********************************************************************
+ * Model structures (ModelType, ModelRange, ModelOptions). These
+ * defines are tied to the pedlib defines wherever applicable. Note:
+ * the ModelOptions structure is part of the pedigree library, while
+ * the other two are defined here.
+ ***********************************************************************/
+#define TP TWOPOINT		/* 2 point */
+#define MP MULTIPOINT		/* multipoint */
+//#define LE LINKAGE_EQUILIBRIUM  /* linkage equilibrium */
+//#define LD LINKAGE_DISEQUILIBRIUM       /* linkage disequilibrium */
+#define SA SEX_AVERAGED
+#define SS SEX_SPECIFIC
+#define DT DICHOTOMOUS		/* dichotomous trait */
+#define QT QUANTITATIVE		/* quantitative trait */
+#define CT COMBINED		/* combined dichotomous/quantitative trait */
+#define MM MARKERTOMARKER	/* marker to marker analysis type */
+#define AM ADJACENTMARKER
+#define ND NORMAL_DISTRIBUTION	/* normal distribution */
+#define TD T_DISTRIBUTION	/* t distribution */
+#define NPENET(x) (((x)*((x)+1))/2)
+
+/* Information about the type of analysis. The array of constants is
+ * only used for some types of QT/CT models (for example, the degrees
+ * of freedom of the T distribution are stored in constants[0]). */
+typedef struct modelType
+{
+  int type;			/* TP, MP: default is 2 point */
+  int numMarkers;		/* number of markers to use for MP */
+  int trait;			/* DT, QT, CT: default is dichotomous */
+  int distrib;			/* Distribution type (QT/CT only) */
+  double mean;			/* mean (QT/CT only) */
+  double sd;			/* standard deviation (QT/CT only) */
+  int *constants;		/* Array of distribution constants (certain QT/CT distributions only) */
+}
+ModelType;
+
+/* LD cell type (used in modelRange below). */
+typedef struct lambdaCell
+{
+  int m;			/* Trait/disease. */
+  int n;			/* Marker. */
+  int ndprime;			/* number of D prime combinations */
+  double ***lambda;		/* Lambda array. */
+}
+LambdaCell;
+
+/* Information about the analysis parameters. */
+typedef struct modelRange
+{
+  double *gfreq;		/* Gene frequencies */
+  int ngfreq;			/* Number of gene frequencies */
+
+  double ***penet;		/* 3D array of penetrance values */
+  int nlclass;			/* Default is 1 liability class */
+  int nalleles;			/* Default is 2 trait alleles D,d */
+  int npenet;			/* Number of penetrance records */
+
+  double ****param;		/* 4D array of extra parameters for QT and CT */
+  int npardim;			/* Number of parameters for given distribution */
+  int nparam;			/* Number of parameter records */
+
+  double **theta;		/* Array of theta arrays */
+  int ngender;			/* Default is sex-averaged */
+  int ntheta;			/* Number of theta records */
+  int *thetacnt;                /* number of theta for male and female */
+
+  /* Currently can only handle SNP marker frequencies. */
+  double *afreq;		/* Allele frequencies */
+  int nafreq;			/* Number of allele frequencies */
+
+  double *alpha;		/* Array of alpha values */
+  int nalpha;			/* Number of alpha values */
+
+  double *tloc;			/* Array of trait locations (multipoint only) */
+  int ntloc;			/* Number of trait locations */
+  int tlmark;			/* Include on-marker trait loci automatically. */
+
+  double **tthresh;		/* Array of trait thresholds (combined traits only) */
+  int ntthresh;			/* Number of trait thresholds */
+
+  /* Currently can only handle two point analysis with LD between
+   * trait locus and marker. */
+  double *dprime;		/* Array of dprime values for LE. */
+  int ndprime;			/* Number of dprime values for LE. */
+
+  /* For linkage disequilibrium: these are the dprime combinations
+   * needed for n and m alleles at the two loci in
+   * disequilibrium. Only used for two point. */
+  struct lambdaCell *lambdas;	/* Cached arrays of lambdas for disequilibrium. */
+  int nlambdas;			/* Number of cached arrays. */
+  int maxnlambdas;		/* Maximum number of cached arrays. */
+}
+ModelRange;
+
+/**********************************************************************
+ * Function prototypes.
+ **********************************************************************/
+int readConfigFile (char *file, ModelType * modelType,
+		    ModelRange * modelRange, ModelOptions * modelOptions);
+void addTraitLocus (ModelRange * range, double val);
+LambdaCell *findLambdas (ModelRange * range, int m, int n);
+void addAlleleFreq (ModelRange * range, double val);
+
+/* summary statistics */
+typedef struct SUMMARY_STAT
+{
+  /* for calculating average */
+  double lr_total;		/* sum of log(LR) */
+  int lr_count;			/* number of models for the (ld, theta) pair */
+  double het_lr_avg;		/* average of HET log(LR) */
+  double het_lr_total;		/* with heterogeneity - alpha, parameter */
+
+  /* for max MOD */
+  double max_lr;		/* max het lr - MOD */
+  double max_alpha;		/* alpha value that maximizes lr */
+  double max_gfreq;		/* gfreq that maximizes lr */
+  int max_penIdx;		/* penetrance that maximizes lr */
+  double max_mf;		/* marker allele frequency that maximizes lr 
+				 * only applies in bi-allelic case */
+  double R_square;		/* only applies in bi-allelic, i.e. SNPs */
+  double max_paramIdx;          /* parameter - SD index */
+  double max_thresholdIdx;      /* threshold index */
+
+  /* for max BR */
+  double max_br_lr;		/* max for BR - with trait parameters integrated out */
+  double max_br_mf;		/* marker allele freq that gives the max_br_lr */
+  double max_br_theta;		/* theta that gives the max BR for the given D prime */
+  double max_br_dprime;		/* dprime gives the overal max BR */
+
+  /* for MP marker list */
+  int *pMarkers;
+  double ppl;			/* imputed MP ppl based on average likelihood ratio */
+  int trait;			/* trait locus index in the locus list for MP */
+} SUMMARY_STAT;
+
+void free_likelihood_storage ();
+/* allocate two point analysis result space */
+int allocate_tp_result_storage ();
+int free_tp_result_storage (int ndprime);
+double calculate_R_square (double p1, double q1, double d);
+/* using the average at each theta to calculate PPL - posterior probability of linkage */
+double calculate_PPL (SUMMARY_STAT ** result);
+/* calculate posterior probability of LD given linakge */
+double calculate_PPLD (SUMMARY_STAT *** result);
+/* get the mean of LR among all D primes for each theta */
+int get_average_LD_LR (SUMMARY_STAT *** result);
+/* integrate out marker allele frequencies and get the max MOD */
+int get_average_LR (SUMMARY_STAT *** result);
+
+/* Model datastructures. modelOptions is defined in the pedigree library. */
+extern ModelType modelType;
+extern ModelRange modelRange;
+extern ModelOptions modelOptions;
+
+/* storage for the NULL likelihood for the multipoint calculation under polynomial */
+extern double ***likelihoodDT;
+extern double *****likelihoodQT;
+
+/* three dimensional array for the two point summary results *
+ * first dimension is the D prime, for LE, D prime=0 with just one element
+ * in this dimension 
+ * second dimension is theta values 
+ * third dimension is marker allele frequency, for LE, only one element in this dimension */
+extern SUMMARY_STAT ***tp_result;
+extern LambdaCell *pLambdaCell;
---
+++ pedlib/polynomial.h
@@ -0,0 +1,224 @@
+#ifndef __POLYNOMIAL_H__
+#define __POLYNOMIAL_H__
+
+#define MAX_POLYNOMIAL_KEY   10000000
+#define CONSTANT_HASH_SIZE 99999
+#define SUM_HASH_SIZE      99999
+#define PRODUCT_HASH_SIZE  99999
+#define FUNCTIONCALL_HASH_SIZE 99999
+#include <stdarg.h>
+
+//This is a global variable used for giving each polynomial an unique ID
+//so that we can know if two polynomials are the same just from their IDs
+int nodeId;
+long nodeIdStamp;
+
+//There are following categories of polynomails
+//T_CONSTANT    : the polynomial represents a constant value for exampel, 0, 1, 1.5 ...
+//T_VARIABLE    : the polynomial represents a variable such as x, y, z, ...
+//T_SUM         : the polynomial represents a sum such as 2x+3y+5.6z+...
+//T_PRODUCT     : the polynomial represents a product such as x^2y^10z^100
+//T_FUNCTIONCALL: the polynomial represents a function call such as log10(x);
+enum expressionType
+{ T_CONSTANT = 0, T_VARIABLE = 1, T_SUM = 2, T_PRODUCT = 3, T_FUNCTIONCALL =
+    4 };
+
+struct polynomial;
+
+//This structure represents the elements of a variable,
+//a variavle has an address in memory, a name
+struct variablePoly
+{
+  char vType;
+  double *vAddrD;
+  int *vAddrI;
+  char vName[100];
+};
+
+//This structure represents the elements of a sum.
+//A sum is composed of a number (saved in num) of items.
+//Each item is a factor (saved in factor) times a polynomial (saved in sum).
+struct sumPoly
+{
+  int num;
+  struct polynomial **sum;
+  double *factor;
+};
+
+//This structure represents the elements of a product.
+//A product is composed of a number (saved in num) of items.
+//Each item includes an exponent (saved in exponent) and a 
+//polynomial (saved in product).
+struct productPoly
+{
+  int num;
+  struct polynomial **product;
+  int *exponent;
+};
+
+//This structure represents the elements of a function call.
+//Each function call is composed of the function name (saved in name)
+// and a number (saved in paraNum) of parameters (saved in para)
+struct functionPoly
+{
+  char *name;
+  int paraNum;
+  struct polynomial **para;
+};
+
+//This structure represents a general polynomial.
+//A polynomial has a unique id, an polynomial type (eType),
+//a value, and the pointer that refers the specific 
+//polynomial structure depending on the polynomial type.
+
+typedef struct polynomial
+{
+  long id;
+  enum expressionType eType;
+  double value;
+//  int                *count;
+//  double              *values;
+  union
+  {
+    struct variablePoly *v;	/*variable */
+    struct sumPoly *s;		/*sum     */
+    struct productPoly *p;	/*product */
+    struct functionPoly *f;	/*function */
+  } e;
+  int valid;
+} Polynomial;
+
+//List is for polynomail evaluation.  When we evaluate a polynomial,
+//it is possible that we evaluate only some parts of it because the
+//other parts have been evaluated when other polynomials are evaluated.
+//List is a structure that records the sub polynomials that need evaluation
+//and the evaluation order of these sub polynomials.
+//listSize is how many sub polynomials this list can hold.
+//listNext is the index for next sub polynomials to be insered in
+//pList is the list of sub polynomials that should be evaluated
+//for the evaluation of the polynomial
+typedef struct polyList
+{
+  int listSize;
+  int listNext;
+  struct polynomial **pList;
+} polynomialList;
+
+//hashStruct is used for speeding up the creation of polynomials.
+//Without hash struct, a polynomial has to compare with all the 
+//currently existing polynomials in this category in order to 
+//know if this polynomial has been created in store.
+//num shows the total number of polynomials that fall into this hash item
+//key saves keys of all the polynomials that fall into this hash item
+//index saves indexes of all the polynomials that fall into this item
+struct hashStruct
+{
+  int num;
+  int *key;
+  int *index;
+};
+
+//We have a polynomials list to save the polynomials in each polynomial category,
+//which is named constantList, variableList, sumList, productList, and functionCallList.
+//The length of these lists are saved in constantCount, variableCount, productCount,
+//functionCallCount respectively.  Each of the lists is dynamically applied since we are
+//not sure how many polynomials we may have.  Therefore, we apply to have a short list
+//initially and increase its length gradually with the increase of the number of 
+//polynomials. The current length of the lists is recorded in constantListLength,
+//variableListLength, sumListLength, productListLength, and functionCallListLength
+//respectively.
+struct polynomial **constantList;
+int constantCount;
+int constantListLength;
+int constantCountStamp;
+
+struct polynomial **variableList;
+int variableCount;
+int variableListLength;
+int variableCountStamp;
+
+struct polynomial **sumList;
+int sumCount;
+int sumListLength;
+int sumCountStamp;
+
+struct polynomial **productList;
+int productCount;
+int productListLength;
+int productCountStamp;
+
+struct polynomial **functionCallList;
+int functionCallCount;
+int functionCallListLength;
+int functionCallCountStamp;
+
+//Hash tables
+//Each category of polynomials has a hash table
+struct hashStruct *constantHash;	//Hash table for constant polynomials
+struct hashStruct *sumHash;	//Hash table for sum polynomials
+struct hashStruct *productHash;	//Hash table for the product polynomials
+struct hashStruct *functionCallHash;	//Hash table for the functionCall polynomials
+
+//The function to evaluate a polynomial recursively
+double evaluateValue (struct polynomial *p);
+
+//Count the numbers of sub polynomials in all categories that resides in the evaluation 
+//list of a polynomial
+void countPoly (struct polyList *l, int *cCounter,
+		int *vCounter, int *sCounter, int *pCounter, int *fCounter);
+//Determine if a polynomial is 0
+int isZeroExp (struct polynomial *p);
+//Determine if a polynomial is 1
+int isOneExp (struct polynomial *p);
+//Determine if a polynomial is -1
+int isMinusOneExp (struct polynomial *p);
+//Print call the constants
+void printConstantList ();
+//Constructor of a constant polynomial
+struct polynomial *constantExp (double con);
+//constructor of a variable polynomial
+struct polynomial *variableExp (double *vD, int *vI, char vType,
+				char name[10]);
+//constructor of a sum polynomial
+struct polynomial *plusExp (int num, ...);
+//constructor of a product polynomial
+struct polynomial *timesExp (int num, ...);
+//constructor of a functionCall polynomial
+struct polynomial *functionCallExp (int num, ...);
+//Print a polynomial
+void expPrinting (struct polynomial *p);
+//Creation and Initialization of evaluation list for a polynomial
+struct polyList *buildPolyList ();
+//Append a sub polynomial to the evaluation list of a polynomial
+void polyListAppend (struct polyList *l, struct polynomial *p);
+//Sort the sub polynomials on the evaluation list of a polynomial.
+//throw away the sub polynomials that doesn't need to be evaluated
+void polyListSorting (struct polynomial *p, struct polyList *l);
+//Sort the sub polynomials on the evaluation list of a polynomial.
+//Keep all the sub polynomials in this polynomial
+void polyListSorting2 (struct polynomial *p, struct polyList *l);
+//Evaluate a polynomial with the help of the evaluation list 
+double evaluatePoly (struct polynomial *pp, struct polyList *l);
+//Print a evaluation list
+void printPolyList (struct polyList *l);
+//record the current status of polynomials
+void makePolynomialStamp ();
+//partially clear the polynomials
+void partialPolynomialClearance ();
+//Initialization before polynomials care created and evaluated.
+void polynomialInitialization ();
+//Clear all data structures related to the polynomials
+void polynomialClearance ();
+//Show polynomials' structures
+void dismantle ();
+//statistics of polynomials
+void polyStatistics (FILE * fp);
+//print out all the variables
+void printAllVariables ();
+//print out all the polynomials
+void printAllPolynomials ();
+
+void polyEvaluationInitialization ();
+void dismantlePolynomialAndSortingList (struct polynomial *p,
+					struct polyList *l);
+#endif
