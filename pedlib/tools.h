979c1cdc7a0ff6ee38705eb41a77641fac04adf0
Jo Valentine-Cooper <jo.valentine-cooper@nationwidechildrens.org>
2019-03-29 15:52:08 +0000

    SVN/Source Directories Reconcile, part 4: much like part 3, but this time for dkelvin, which got merged into regular kelvin eventually and so arguably has a place here

---
+++ pedlib/tools.h
@@ -0,0 +1,14 @@
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+/* number of bits in an integer */
+#define INT_BITS		(sizeof(int)*8)
+
+int is_line_blank_or_comment (char *line);
+int is_line_blank (char *line);
+int is_line_comment (char *line);
+char *get_nonblank_line (char *pLine, int maxLen, FILE * fp, int *pLineNo);
+char *fgetlongs (char **buff, int *bufflen, FILE * fp);
+
+
+#endif
